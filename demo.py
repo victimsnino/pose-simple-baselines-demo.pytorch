073d77d7909125702c81fe51d8e85747dbfb37b7
Victimsnino <victimsnino@gmail.com>
2019-04-24 10:00:11 +0300

    #2

--- demo.py
+++ demo.py
@@ -13,250 +13,13 @@ import numpy as np
 import yaml
 from easydict import EasyDict as edict
 
+import model as m
 
 #model
-DECONV_WITH_BIAS = False
-NUM_DECONV_LAYERS = 3
-NUM_DECONV_FILTERS = [256, 256, 256]
-NUM_DECONV_KERNELS = [4, 4, 4]
-FINAL_CONV_KERNEL = 1
-NUM_JOINTS = 16
-BN_MOMENTUM = 0.1
 IMAGE_SIZE = [256, 256]
 
 image = np.empty(())
 
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
-
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=BN_MOMENTUM)
-        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1,
-                               bias=False)
-        self.bn3 = nn.BatchNorm2d(planes * self.expansion,
-                                  momentum=BN_MOMENTUM)
-        self.relu = nn.ReLU(inplace=True)
-        self.downsample = downsample
-        self.stride = stride
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class PoseResNet(nn.Module):
-
-    def __init__(self, block, layers, **kwargs):
-        self.inplanes = 64
-        self.deconv_with_bias = DECONV_WITH_BIAS
-
-        super(PoseResNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64, momentum=BN_MOMENTUM)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-
-        # used for deconv layers
-        self.deconv_layers = self._make_deconv_layer(
-            NUM_DECONV_LAYERS,
-            NUM_DECONV_FILTERS,
-            NUM_DECONV_KERNELS,
-        )
-
-        self.final_layer = nn.Conv2d(
-            in_channels=NUM_DECONV_FILTERS[-1],
-            out_channels=NUM_JOINTS,
-            kernel_size=FINAL_CONV_KERNEL,
-            stride=1,
-            padding=1 if FINAL_CONV_KERNEL == 3 else 0
-        )
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion, momentum=BN_MOMENTUM),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def _get_deconv_cfg(self, deconv_kernel, index):
-        if deconv_kernel == 4:
-            padding = 1
-            output_padding = 0
-        elif deconv_kernel == 3:
-            padding = 1
-            output_padding = 1
-        elif deconv_kernel == 2:
-            padding = 0
-            output_padding = 0
-
-        return deconv_kernel, padding, output_padding
-
-    def _make_deconv_layer(self, num_layers, num_filters, num_kernels):
-        assert num_layers == len(num_filters), \
-            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
-        assert num_layers == len(num_kernels), \
-            'ERROR: num_deconv_layers is different len(num_deconv_filters)'
-
-        layers = []
-        for i in range(num_layers):
-            kernel, padding, output_padding = \
-                self._get_deconv_cfg(num_kernels[i], i)
-
-            planes = num_filters[i]
-            layers.append(
-                nn.ConvTranspose2d(
-                    in_channels=self.inplanes,
-                    out_channels=planes,
-                    kernel_size=kernel,
-                    stride=2,
-                    padding=padding,
-                    output_padding=output_padding,
-                    bias=self.deconv_with_bias))
-            layers.append(nn.BatchNorm2d(planes, momentum=BN_MOMENTUM))
-            layers.append(nn.ReLU(inplace=True))
-            self.inplanes = planes
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.deconv_layers(x)
-        x = self.final_layer(x)
-
-        return x
-
-
-            
-resnet_spec = {18: (BasicBlock, [2, 2, 2, 2]),
-               34: (BasicBlock, [3, 4, 6, 3]),
-               50: (Bottleneck, [3, 4, 6, 3]),
-               101: (Bottleneck, [3, 4, 23, 3]),
-               152: (Bottleneck, [3, 8, 36, 3])}
-               
-def get_pose_net(layers, is_train, **kwargs):
-    num_layers = layers
-    
-    block_class, layers = resnet_spec[num_layers]
-    model = PoseResNet(block_class, layers, **kwargs)
-
-    return model
-
-# predication
-def get_max_preds(batch_heatmaps):
-    '''
-    get predictions from score maps
-    heatmaps: numpy.ndarray([batch_size, num_joints, height, width])
-    '''
-    assert isinstance(batch_heatmaps, np.ndarray), \
-        'batch_heatmaps should be numpy.ndarray'
-    assert batch_heatmaps.ndim == 4, 'batch_images should be 4-ndim'
-
-    batch_size = batch_heatmaps.shape[0]
-    num_joints = batch_heatmaps.shape[1]
-    width = batch_heatmaps.shape[3]
-    heatmaps_reshaped = batch_heatmaps.reshape((batch_size, num_joints, -1))
-    idx = np.argmax(heatmaps_reshaped, 2)
-    maxvals = np.amax(heatmaps_reshaped, 2)
-
-    maxvals = maxvals.reshape((batch_size, num_joints, 1))
-    idx = idx.reshape((batch_size, num_joints, 1))
-
-    preds = np.tile(idx, (1, 1, 2)).astype(np.float32)
-
-    preds[:, :, 0] = (preds[:, :, 0]) % width
-    preds[:, :, 1] = np.floor((preds[:, :, 1]) / width)
-
-    pred_mask = np.tile(np.greater(maxvals, 0.0), (1, 1, 2))
-    pred_mask = pred_mask.astype(np.float32)
-
-    preds *= pred_mask
-    return preds, maxvals
- 
  # main and args
 def parse_args():
     parser = argparse.ArgumentParser(description='Train keypoints network')
@@ -353,7 +116,7 @@ def main():
     if args.min_confidence_threshold:
         min_confidence_threshold = np.float(args.min_confidence_threshold)
         
-    model = eval('get_pose_net')(
+    model = eval('m.get_pose_net')(
         num_layers, is_train=False
     )
     
@@ -417,7 +180,7 @@ def main():
         with torch.no_grad():
             # compute output heatmap
             output = model(input)
-            coords, maxvals = get_max_preds(output.clone().cpu().numpy())
+            coords, maxvals = m.get_max_preds(output.clone().cpu().numpy())
             print(maxvals)
             cv2.waitKey(1000) & 0xFF
             image = data_numpy.copy()
@@ -463,7 +226,7 @@ def main():
             with torch.no_grad():
                 # compute output heatmap
                 output = model(input)
-                coords, maxvals = get_max_preds(output.clone().cpu().numpy())
+                coords, maxvals = m.get_max_preds(output.clone().cpu().numpy())
                 image = data_numpy.copy()
                 badPoints = 0
                 for i in range(coords[0].shape[0]):
