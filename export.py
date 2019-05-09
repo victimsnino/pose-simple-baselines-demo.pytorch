c104e143c9e64e2248fa3e33f9ad9cf287fb8256
Victimsnino <victimsnino@gmail.com>
2019-05-09 10:30:41 +0300

    fix export script

--- export.py
+++ export.py
@@ -3,124 +3,69 @@ import os
 import pprint
 
 import torch
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.optim
-import torch.utils.data
-import torch.utils.data.distributed
-
-import _init_paths
-from core.config import config
-from utils.utils import create_logger
+import torchvision.transforms as transforms
+import torch.nn as nn
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
 
-import dataset
-import models
+import yaml
+from easydict import EasyDict as edict
 
+import model as mod
 
+#model
+IMAGE_SIZE = [256, 256]
+
+image = np.empty(())
+
+ # main and args
 def parse_args():
     parser = argparse.ArgumentParser(description='Train keypoints network')
-    # general
+    
     parser.add_argument('--cfg',
-                        help='experiment configure file name',
+                        help='Path to cfg *.yaml',
                         required=True,
                         type=str)
-
-    args, rest = parser.parse_known_args()
-    # update config
-    update_config(args.cfg)
-
-    # training
-    parser.add_argument('--frequent',
-                        help='frequency of logging',
-                        default=config.PRINT_FREQ,
-                        type=int)
-    parser.add_argument('--gpus',
-                        help='gpus',
-                        type=str)
-    parser.add_argument('--workers',
-                        help='num of dataloader workers',
-                        type=int)
-    parser.add_argument('--model-file',
-                        help='model state file',
-                        type=str)
-    parser.add_argument('--use-detect-bbox',
-                        help='use detect bbox',
-                        action='store_true')
-    parser.add_argument('--flip-test',
-                        help='use flip test',
-                        action='store_true')
-    parser.add_argument('--post-process',
-                        help='use post process',
-                        action='store_true')
-    parser.add_argument('--shift-heatmap',
-                        help='shift heatmap',
-                        action='store_true')
-    parser.add_argument('--coco-bbox-file',
-                        help='coco detection bbox file',
-                        type=str)
-
     args = parser.parse_args()
 
     return args
 
+def loadConfig(config_file):
+    numLayers = 0 
+    size = ()
+    model_file = ''
+    with open(config_file) as f:
+        exp_config = edict(yaml.load(f))
+        for k, v in exp_config.items():
+            if k == 'PRETRAINED':
+                model_file = v
+            if k == 'NUM_LAYERS':
+                numLayers= v
+            if k == 'IMAGE_SIZE':
+                size = (v[0], v[1])
+    return model_file, numLayers, size
+  
 
-def reset_config(config, args):
-    if args.gpus:
-        config.GPUS = args.gpus
-    if args.workers:
-        config.WORKERS = args.workers
-    if args.use_detect_bbox:
-        config.TEST.USE_GT_BBOX = not args.use_detect_bbox
-    if args.flip_test:
-        config.TEST.FLIP_TEST = args.flip_test
-    if args.post_process:
-        config.TEST.POST_PROCESS = args.post_process
-    if args.shift_heatmap:
-        config.TEST.SHIFT_HEATMAP = args.shift_heatmap
-    if args.model_file:
-        config.TEST.MODEL_FILE = args.model_file
-    if args.coco_bbox_file:
-        config.TEST.COCO_BBOX_FILE = args.coco_bbox_file
-
-
-def main():
+def main():  
     args = parse_args()
-    reset_config(config, args)
-
-    logger, final_output_dir, tb_log_dir = create_logger(
-        config, args.cfg, 'valid')
-
-    logger.info(pprint.pformat(args))
-    logger.info(pprint.pformat(config))
-
-    # cudnn related setting
-    cudnn.benchmark = config.CUDNN.BENCHMARK
-    torch.backends.cudnn.deterministic = config.CUDNN.DETERMINISTIC
-    torch.backends.cudnn.enabled = config.CUDNN.ENABLED
-
-    model = eval('models.'+config.MODEL.NAME+'.get_pose_net')(
-        config, is_train=False
+    model_file, num_layers, IMAGE_SIZE = loadConfig(args.cfg)
+    
+    model = eval('mod.get_pose_net')(
+        num_layers, is_train=False
     )
 
-    print('Here')
-    if config.TEST.MODEL_FILE:
-        logger.info('=> loading model from {}'.format(config.TEST.MODEL_FILE))
-        model.load_state_dict(torch.load(config.TEST.MODEL_FILE))
-    else:
-        model_state_file = os.path.join(final_output_dir,
-                                        'final_state.pth.tar')
-        logger.info('=> loading model from {}'.format(model_state_file))
-        model.load_state_dict(torch.load(model_state_file))
-
-    gpus = [int(i) for i in config.GPUS.split(',')]
+    if model_file:
+        print('=> loading model from {}'.format(model_file))
+        model.load_state_dict(torch.load(model_file))
     
-    dummy_input = torch.randn(1, 3, config.MODEL.IMAGE_SIZE[0], config.MODEL.IMAGE_SIZE[1])
+    dummy_input = torch.randn(1, 3,IMAGE_SIZE[0], IMAGE_SIZE[1])
     
     input_names = [ "input_1" ]
     output_names = [ "output1" ]
 
-    torch.onnx.export(model, dummy_input, config.TEST.MODEL_FILE+".onnx", verbose=True, input_names=input_names, output_names=output_names)
+    torch.onnx.export(model, dummy_input, model_file+".onnx", verbose=True, input_names=input_names, output_names=output_names)
 
     print('END')
 
